70b2385977b82f34834ad7039d2ecab19b9e7d94
jylei <jlei91@gmail.com>
2017-11-12 10:39:14 -0800

    comment out3000

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ exports.TEST_DATABASE_URL =  process.env.TEST_DATABASE_URL ||
                             'mongodb://localhost/test-recipe-app';
 exports.PORT = process.env.PORT || 8080;
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secretkey';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
